f817a1ad9a54e8bd2487311cab48c4caa81c7b42
onewesong <onewesong@gmail.com>
2025-04-28 14:10:44 +0800

    update

--- pages/iframe.py
+++ pages/iframe.py
@@ -3,3 +3,7 @@ import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
 components.iframe("https://example.com", height=500)
+
+# 内部路由
+components.iframe("/chat", height=500)
+
